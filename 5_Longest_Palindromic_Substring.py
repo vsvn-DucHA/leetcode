987e104d5bc4dfab98c2bafbb6afe85c4e04b4a3
hoang anh duc <anhduc.hoang@veriserve-vietnam.com>
2023-11-24 16:59:47 +0700

    22/10/2023

---
+++ 5_Longest_Palindromic_Substring.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if s == s[::-1]: return s
+        
+        start, size = 0, 1
+        for i in range(1, len(s)):
+            l, r = i - size, i + 1
+            s1, s2 = s[l-1:r], s[l:r]
+            print(i,'a',s1,s2,size)
+            if l - 1 >= 0 and s1 == s1[::-1]:
+                start, size = l - 1, size + 2
+            elif s2 == s2[::-1]:
+                start, size = l, size + 1
+            print(i,'b',s1,s2,size)
+            print()
+        return s[start:start+size]
+        
+s="abcde"
+so=Solution()
+# print(so.longestPalindrome(s))
+print(s[::-1])
